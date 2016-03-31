88b63b86cf5db32b8a0e01651fc115292a2dc31b
larslemos <larslemos@gmail.com>
2016-03-31 03:27:04 -0700

    Initial commit

---
+++ src/main/java/com/twair/SearchForm.java
@@ -0,0 +1,41 @@
+package com.twair;
+
+public class SearchForm {
+    private String from;
+    private String to;
+    private String number;
+
+    public int getnPassengers() {
+        return nPassengers;
+    }
+
+    public void setnPassengers(int nPassengers) {
+        this.nPassengers = nPassengers;
+    }
+
+    private int nPassengers;
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
